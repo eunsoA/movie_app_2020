49a218981a480ef1651d54faf9a8aaa541872b7f
eunso <esahn01@naver.com>
2020-12-23 19:24:57 +0900

    9

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div> Hello!!!!!</div>;
-  //<div className="App" />;
+  return (
+    <div>
+      <h1>HELLO</h1>
+    </div>
+  );
 }
 
 export default App;
